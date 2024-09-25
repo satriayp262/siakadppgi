cca90299fffea25cd55cf91ae9f713fec79ab110
AndikaBastian <andikabastian20@gmail.com>
2024-09-25 14:01:07 +0700

    sd

--- app/Livewire/Admin/Dashboard/Index.php
+++ app/Livewire/Admin/Dashboard/Index.php
@@ -3,11 +3,22 @@
 namespace App\Livewire\Admin\Dashboard;
 
 use Livewire\Component;
+use App\Models\Matakuliah;
+use App\Models\Prodi;
 
 class Index extends Component
 {
+    public $matakuliah;
+    public $prodi;
 
+    public function mount()
+    {
+        $matkul = Matakuliah::query()->count();
+        $this->matakuliah = $matkul;
 
+        $prodis = Prodi::query()->count();
+        $this->prodi = $prodis;
+    }
 
     public function render()
     {
