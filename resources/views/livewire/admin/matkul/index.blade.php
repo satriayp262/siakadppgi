4ccbbf43d8f0e31364c0c975c295c6304bc4bff4
AndikaBastian <andikabastian20@gmail.com>
2024-09-25 14:48:43 +0700

    Update index.blade.php

--- resources/views/livewire/admin/matkul/index.blade.php
+++ resources/views/livewire/admin/matkul/index.blade.php
@@ -1,6 +1,30 @@
 <div class="mx-5">
+    <div class="flex items-center justify-between">
+            <nav aria-label="Breadcrumb">
+                <ol class="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                    <li aria-current="page">
+                        <div class="flex items-center">
+                            <svg class="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="text-sm font-medium text-gray-500 ms-1 md:ms-2">Mata Kuliah</span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+
+            <div class="text-right">
+                <ol class="breadcrumb">
+                    <li class="text-sm font-medium text-gray-700 breadcrumb-item">
+                        {{ \Carbon\Carbon::now()->locale('id')->isoFormat('dddd, D MMMM Y') }}
+                    </li>
+                </ol>
+            </div>
+        </div>
     <div class="flex flex-col justify-between mx-4 mt-4">
-        <h1 class="text-2xl font-bold ">Berita Table</h1>
+        <h1 class="text-2xl font-bold ">Table Mata Kuliah</h1>
         <div>
             @if (session()->has('message'))
                 <div id="flash-message"
