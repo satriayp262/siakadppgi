0eb145e3b756d678b4f82784b1b3489d78bbd4f3
AndikaBastian <andikabastian20@gmail.com>
2024-09-26 10:31:41 +0700

    ldf

--- resources/views/livewire/admin/matkul/index.blade.php
+++ resources/views/livewire/admin/matkul/index.blade.php
@@ -1,28 +1,4 @@
 <div class="mx-5">
-    <div class="flex items-center justify-between">
-            <nav aria-label="Breadcrumb">
-                <ol class="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <li aria-current="page">
-                        <div class="flex items-center">
-                            <svg class="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 9 4-4-4-4" />
-                            </svg>
-                            <span class="text-sm font-medium text-gray-500 ms-1 md:ms-2">Mata Kuliah</span>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
-
-            <div class="text-right">
-                <ol class="breadcrumb">
-                    <li class="text-sm font-medium text-gray-700 breadcrumb-item">
-                        {{ \Carbon\Carbon::now()->locale('id')->isoFormat('dddd, D MMMM Y') }}
-                    </li>
-                </ol>
-            </div>
-        </div>
     <div class="flex flex-col justify-between mx-4 mt-4">
         <h1 class="text-2xl font-bold ">Table Mata Kuliah</h1>
         <div>
@@ -65,12 +41,12 @@
                     <td class="px-4 py-2 text-center">
                         <div class="flex items-center">
                             <div class="flex space-x-2">
-                                <livewire:admin.matkul.edit :id_mata_kuliah="$matkul->id_mata_kuliah" wire:key="edit-{{ $matkul->id_mata_kuliah }}" />
+                                <livewire:admin.matkul.edit :id_mata_kuliah="$matkul->id_mata_kuliah" wire:key="edit-{{rand().$matkul->id_mata_kuliah }}" />
                                 <button class="inline-block px-3 py-1 ml-2 text-white bg-red-500 rounded hover:bg-red-700"
                                         onclick="confirmDelete({{ $matkul->id_mata_kuliah }}, '{{ $matkul->nama_mata_kuliah }}')">Delete</button>
                             </div>
                             <div class="flex mt-2 space-x-2">
-                                <livewire:admin.matkul.selengkapnya :id_mata_kuliah="$matkul->id_mata_kuliah" wire:key="selengkapnya-{{ $matkul->id_mata_kuliah }}" />
+                                <livewire:admin.matkul.selengkapnya :id_mata_kuliah="$matkul->id_mata_kuliah" wire:key="selengkapnya-{{rand(). $matkul->id_mata_kuliah }}" />
                             </div>
                         </div>
                     </td>
@@ -79,7 +55,7 @@
         </tbody>
     </table>
     <!-- Pagination Controls -->
-    <div class="py-8 mt-4 text-center">
+    <div class="mt-4 text-center">
         {{ $matkuls->links('pagination::tailwind') }}
     </div>
     <script>
