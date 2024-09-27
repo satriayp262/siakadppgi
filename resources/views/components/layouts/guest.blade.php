f2c88bee3eb4d866573a4e975d5a3490366a34cc
AndikaBastian <andikabastian20@gmail.com>
2024-09-27 13:57:14 +0700

    Update guest.blade.php

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -33,6 +33,7 @@
     </style>
     @vite('resources/css/app.css')
     <title>SIAKAD {{ $title ?? 'PPGI' }}</title>
+    <link rel="icon" href="{!! asset('img/piksi.png') !!}" />
     @livewireStyles
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 
