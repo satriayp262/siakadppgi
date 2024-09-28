f337f18749a1a98aaff29bf30e1e33c82c3cbb65
Fajaraaja <fajardwi881117@gmail.com>
2024-09-28 15:17:43 +0700

    Refactor: Update pagination links and search functionality in Admin Kelas Index

--- app/Livewire/Admin/Kelas/Index.php
+++ app/Livewire/Admin/Kelas/Index.php
@@ -3,11 +3,39 @@
 namespace App\Livewire\Admin\Kelas;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Kelas;
 
 class Index extends Component
 {
+
+    use WithPagination;
+    public $search = '';
+
+    // public function destroy($kode_kelas)
+    // {
+    //     $kelas = Kelas::find($kode_kelas);
+
+    //     // Hapus data kelas
+    //     $kelas->delete();
+
+    //     // Tampilkan pesan sukses
+    //     session()->flash('message', 'Kelas Berhasil di Hapus');
+    //     session()->flash('message_type', 'error');
+    // }
+
+
+
+
     public function render()
     {
-        return view('livewire.admin.kelas.index');
+        $kelas = Kelas::query()
+            ->where('kode_kelas', 'like', '%' . $this->search . '%')
+            ->orWhere('nama_kelas', 'like', '%' . $this->search . '%')
+            ->latest()
+            ->paginate(5);
+        return view('livewire.admin.kelas.index', [
+            'kelas' => $kelas,
+        ]);
     }
 }
