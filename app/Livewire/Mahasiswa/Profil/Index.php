2f9e3c6b220fe745236b651a8fa2f310bf08ba19
Fajaraaja <fajardwi881117@gmail.com>
2024-09-29 15:03:31 +0700

    Refactor: Update layout and add profile page for Mahasiswa

---
+++ app/Livewire/Mahasiswa/Profil/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Mahasiswa\Profil;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.mahasiswa.profil.index');
+    }
+}
---
+++ resources/views/livewire/mahasiswa/profil/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>ini halaman profil</h1>
+</div>
