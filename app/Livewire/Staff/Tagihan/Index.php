eb180f5e85d3af6f5455773d04ce34849bc69ddf
Fajaraaja <fajardwi881117@gmail.com>
2024-09-30 15:41:07 +0700

    Feat : add foreign to semester table

--- app/Livewire/Staff/Tagihan/Index.php
+++ app/Livewire/Staff/Tagihan/Index.php
@@ -3,11 +3,26 @@
 namespace App\Livewire\Staff\Tagihan;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Tagihan;
+use App\Models\Semester;
 
 class Index extends Component
 {
+    use WithPagination;
+
+
     public function render()
     {
-        return view('livewire.staff.tagihan.index');
+        $semesters = Semester::all();
+        $tagihans = Tagihan::query()
+
+            ->latest()
+            ->paginate(5);
+
+        return view('livewire.staff.tagihan.index', [
+            'tagihans' => $tagihans,
+            'semesters' => $semesters
+        ]);
     }
 }
