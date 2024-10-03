0f816546e15c49255c9513ca15efadcb034a1303
Fajaraaja <fajardwi881117@gmail.com>
2024-10-03 14:21:37 +0700

    Refactor: Update sorting functionality in staff tagihan index

--- resources/views/livewire/staff/tagihan/index.blade.php
+++ resources/views/livewire/staff/tagihan/index.blade.php
@@ -1,4 +1,102 @@
 <div>
-    <h1>Semester saat ini</h1>
-    <h1>{{ $semesters[0]->nama_semester }}</h1>
-</div>
+    <div class="mx-5">
+        <div class="flex flex-col justify-between mx-4 mt-4">
+            <h1>Semester Saat ini :</h1>
+            {{ $semesters[0]->nama_semester }}
+            <div>
+                @if (session()->has('message'))
+                    @php
+                        $messageType = session('message_type', 'success'); // Default to success
+                        $bgColor =
+                            $messageType === 'error'
+                                ? 'bg-red-500'
+                                : ($messageType === 'warning'
+                                    ? 'bg-blue-500'
+                                    : 'bg-green-500');
+                    @endphp
+                    <div id="flash-message"
+                        class="flex items-center justify-between p-4 mx-12 mt-8 mb-4 text-white {{ $bgColor }} rounded">
+                        <span>{{ session('message') }}</span>
+                        <button class="p-1" onclick="document.getElementById('flash-message').remove();"
+                            class="font-bold text-white">
+                            &times;
+                        </button>
+                    </div>
+                    {{-- @push('scripts')
+                    <script>
+                        setTimeout(() => {
+                            const flashMessage = document.getElementById('flash-message');
+                            if (flashMessage) {
+                                flashMessage.remove();
+                            }
+                        }, 1000); // Adjust the time (in milliseconds) as needed
+                    </script>
+                @endpush --}}
+                @endif
+            </div>
+            <!-- Modal Form -->
+            <div class="flex justify-between mt-2">
+                {{-- <livewire:admin.prodi.create /> --}}
+                <input type="text" wire:model.live="search" placeholder="   Search"
+                    class="px-2 ml-4 border border-gray-300 rounded-lg">
+            </div>
+        </div>
+        <table class="min-w-full mt-4 bg-white border border-gray-200">
+            <thead>
+                <tr class="items-center w-full text-sm text-white align-middle bg-gray-800">
+                    <th class="px-4 py-2 text-center">No.</th>
+                    <th class="px-4 py-2 text-center">Nama Mahasiswa</th>
+                    <th class="px-4 py-2 text-center">NIM</th>
+                    <th class="px-4 py-2 text-center">Prodi
+                        <!-- resources/views/components/sortable-column.blade.php -->
+                        @props(['field', 'sortField', 'sortDirection'])
+
+                        <a href="#" wire:click.prevent="sortBy('kode_prodi')">
+                            <svg class="inline w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 24 24">
+                                <path
+                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg>
+                        </a>
+                    </th>
+                    <th class="px-4 py-2 text-center">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($mahasiswas as $mahasiswa)
+                    <tr class="border-t" wire:key="mahasiswa-{{ $mahasiswa->id }}">
+                        <td class="px-4 py-2 text-center">{{ $loop->iteration }}</td>
+                        <td class="px-4 py-2 text-center">{{ $mahasiswa->nama }}</td>
+                        <td class="px-4 py-2 text-center">{{ $mahasiswa->NIM }}</td>
+                        <td class="px-4 py-2 text-center">{{ $mahasiswa->prodi->nama_prodi }}</td>
+                        <td class="px-4 py-2 text-center">
+                            <button class="inline-block px-3 py-1 text-white bg-blue-500 rounded hover:bg-blue-700"
+                                wire:click="buatTagihan({{ $mahasiswa->id }})">Buat Tagihan</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <!-- Pagination Controls -->
+        <div class="py-8 mt-4 text-center">
+            {{-- {{ $tagihans->links('') }} --}}
+        </div>
+
+        <script>
+            function confirmDelete(id, nama_prodi) {
+                Swal.fire({
+                    title: `Apakah anda yakin ingin menghapus Prodi ${nama_prodi}?`,
+                    text: "Data yang telah dihapus tidak dapat dikembalikan!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#6c757d',
+                    confirmButtonText: 'Hapus'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        @this.call('destroy', id);
+                    }
+                });
+            }
+        </script>
+    </div>
