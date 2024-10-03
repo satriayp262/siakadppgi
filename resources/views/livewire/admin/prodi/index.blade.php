d22db96d0daef0c35cea063e4d3e93bee364b730
Fajaraaja <fajardwi881117@gmail.com>
2024-10-03 13:37:06 +0700

    Refactor : Update edit text,etc into icon

--- resources/views/livewire/admin/prodi/index.blade.php
+++ resources/views/livewire/admin/prodi/index.blade.php
@@ -58,10 +58,18 @@
                     <td class="px-4 py-2 text-center w-1/4">{{ $prodi->nama_prodi }}</td>
                     <td class="px-4 py-2 text-center w-1/4">{{ $prodi->jenjang }}</td>
                     <td class="px-4 py-2 text-center w-1/2">
-                        <div class="flex items-center justify-center space-x-2">
+                        <div class="flex justify-center space-x-2">
                             <livewire:admin.prodi.edit :id_prodi="$prodi->id_prodi" wire:key="edit-{{ $prodi->id_prodi }}" />
-                            <button class="inline-block px-3 py-1 text-white bg-red-500 rounded hover:bg-red-700"
-                                onclick="confirmDelete('{{ $prodi->id_prodi }}', '{{ $prodi->nama_prodi }}')">Delete</button>
+                            <button class="inline-block px-4 py-1 text-white bg-red-500 rounded hover:bg-red-700"
+                                onclick="confirmDelete('{{ $prodi->id_prodi }}', '{{ $prodi->nama_prodi }}')"><svg
+                                    class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                </svg>
+                            </button>
                         </div>
                     </td>
                 </tr>
