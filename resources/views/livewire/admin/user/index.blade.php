de9deca28930ca27617e391e15d4e912503db33e
Fajaraaja <fajardwi881117@gmail.com>
2024-10-03 14:22:10 +0700

    Refactor : update user index

--- resources/views/livewire/admin/user/index.blade.php
+++ resources/views/livewire/admin/user/index.blade.php
@@ -71,11 +71,17 @@
                         </span>
                     </td>
                     <td class="px-4 py-2 text-center w-1/2">
-                        <div class="flex items-center justify-center space-x-2">
+                        <div class="flex justify-center space-x-2">
                             <livewire:admin.user.edit :id="$user->id" wire:key="edit-{{ $user->id }}" />
-                            <button class="inline-block px-3 py-1 text-white bg-red-500 rounded hover:bg-red-700"
+                            <button class="inline-block px-4 py-1 text-white bg-red-500 rounded hover:bg-red-700"
                                 onclick="confirmDelete('{{ $user->id }}', '{{ $user->name }}')">
-                                Delete
+                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                </svg>
                             </button>
                         </div>
                     </td>
