9e13439ca59cb6d1052b59ddc3a1a38a2e7dacb1
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2024-10-03 13:53:04 +0700

    Update Edit.php

--- app/Livewire/Admin/Dosen/Edit.php
+++ app/Livewire/Admin/Dosen/Edit.php
@@ -14,7 +14,7 @@ class Edit extends Component
     {
         return [
             'nama_dosen' => 'required|string|max:255',
-            'nidn' => 'required|string|min:10|max:10|unique:dosen' . $this->id_dosen . ',id_dosen',
+            'nidn' => 'required|string|min:10|max:10|unique:dosen,nidn,' . $this->id_dosen,
             'jenis_kelamin' => 'required|in:laki-laki,perempuan',
             'jabatan_fungsional' => 'required|string|max:255',
             'kepangkatan' => 'required|string|max:255',
@@ -24,8 +24,7 @@ class Edit extends Component
 
     public function clear($id_dosen)
     {
-        $this->reset();
-        $this->dispatch('refreshComponent');
+        $this->resetExcept('prodi');
         $dosen = Dosen::find($id_dosen);
         if ($dosen) {
             $this->id_dosen = $dosen->id_dosen;
@@ -36,31 +35,12 @@ class Edit extends Component
             $this->kepangkatan = $dosen->kepangkatan;
             $this->kode_prodi = $dosen->kode_prodi;
         }
-    }
-
-    protected $listeners = ['refreshComponent' => '$refresh'];
-    public function placeholder()
-    {
-        return <<<'BLADE'
-        <div>
-            <h5 class="card-title placeholder-glow">
-                <span class="placeholder col-4"></span>
-            </h5>
-            <p class="card-text placeholder-glow">
-                <span class="placeholder col-7"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-6"></span>
-                <span class="placeholder col-8"></span>
-            </p>
-        </div>
-        BLADE;
     }
 
     public function mount($id_dosen)
     {
         $dosen = Dosen::find($id_dosen);
-        $this->prodi = Prodi::all();
+        $this->prodi = Prodi::all() ?? collect();
         if ($dosen) {
             $this->id_dosen = $dosen->id_dosen;
             $this->nama_dosen = $dosen->nama_dosen;
@@ -70,35 +50,20 @@ class Edit extends Component
             $this->kepangkatan = $dosen->kepangkatan;
             $this->kode_prodi = $dosen->kode_prodi;
         }
-        return $dosen;
     }
 
     public function update()
     {
-
-        $validatedData = $this->validate([
-            'nama_dosen' => 'required|string|max:255',
-            'nidn' => 'required|string|min:10|max:10',
-            'jenis_kelamin' => 'required|in:laki-laki,perempuan',
-            'jabatan_fungsional' => 'required|string|max:255',
-            'kepangkatan' => 'required|string|max:255',
-            'kode_prodi' => 'required|string|max:255',
-        ]);
+        $validatedData = $this->validate();
 
         $dosen = Dosen::find($this->id_dosen);
-        $dosen->update([
-            'nama_dosen' => $validatedData['nama_dosen'],
-            'nidn' => $validatedData['nidn'],
-            'jenis_kelamin' => $validatedData['jenis_kelamin'],
-            'jabatan_fungsional' => $validatedData['jabatan_fungsional'],
-            'kepangkatan' => $validatedData['kepangkatan'],
-            'kode_prodi' => $validatedData['kode_prodi'],
-        ]);
+        if ($dosen) {
+            $dosen->update($validatedData);
 
-        // Reset form dan dispatch event
-        $this->resetExcept('prodi');
-        $this->dispatch('dosenUpdated');
-        return $dosen;
+            // Reset form except 'prodi' and dispatch browser event
+            $this->resetExcept('prodi');
+            $this->dispatch('dosenUpdated');
+        }
     }
 
     public function render()
@@ -106,3 +71,4 @@ class Edit extends Component
         return view('livewire.admin.dosen.edit');
     }
 }
+
