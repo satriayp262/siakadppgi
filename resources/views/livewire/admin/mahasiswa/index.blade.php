ff2c93341cac1d479f84fa72fd70488a8598bf24
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-10-14 16:37:42 +0700

    fix : download template mahasiswa

--- resources/views/livewire/admin/mahasiswa/index.blade.php
+++ resources/views/livewire/admin/mahasiswa/index.blade.php
@@ -100,25 +100,31 @@
                                             <div class="flex flex-col">
                                                 <label class="block text-sm font-medium text-gray-700">Template
                                                     Dokumen</label>
-                                                <button wire:click="downloadTemplate"
-                                                    class="flex justify-between items-center w-full px-2 py-1 mt-1 text-sm bg-gray-200 rounded-md border-gray-700 shadow-2xl focus:border-indigo-500 sm:text-sm">
-                                                    <svg class="w-6 h-6 text-gray-500"
+                                                <a href="{{ asset('template/Mahasiswi.xlsx') }}"
+                                                    class="flex items-center justify-between w-full px-2 py-1 mt-1 text-sm bg-gray-200 border-gray-700 rounded-md shadow-2xl focus:border-indigo-500 sm:text-sm">
+
+                                                    <!-- Left icon -->
+                                                    <svg class="w-6 h-6 text-gray-500" aria-hidden="true"
                                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                         fill="none" viewBox="0 0 24 24">
                                                         <path fill="currentColor"
                                                             d="M9 2.221V7H4.221a2 2 0 0 1 .365-.5L8.5 2.586A2 2 0 0 1 9 2.22ZM11 2v5a2 2 0 0 1-2 2H4v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-7Z"
                                                             clip-rule="evenodd" />
                                                     </svg>
+
+                                                    <!-- Button text -->
                                                     <span
-                                                        class="text-left flex-grow px-2 text-black-500 font-medium">Template_Mahasiswa.xlsx</span>
-                                                    <svg class="w-6 h-6 text-gray-500"
+                                                        class="flex-grow px-2 font-medium text-left text-black-500">Template_Import_Mahasiswa.xlsx</span>
+
+                                                    <!-- Right icon -->
+                                                    <svg class="w-6 h-6 text-gray-500" aria-hidden="true"
                                                         xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24">
                                                         <path stroke="currentColor" stroke-linecap="round"
                                                             stroke-linejoin="round" stroke-width="2"
                                                             d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2m-8 1V4m0 12-4-4m4 4 4-4" />
                                                     </svg>
-                                                </button>
+                                                </a>
                                                 <br>
                                                 <label for="file"
                                                     class="block text-sm font-medium text-gray-700">File</label>
