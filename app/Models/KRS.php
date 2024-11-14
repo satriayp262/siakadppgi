63d485bfe17a4d491bf3398a936b89d3291c19fa
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-11-14 11:56:40 +0700

    Feat : KRS migration

---
+++ app/Models/KRS.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KRS extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'id_krs';
+    protected $table = 'krs';
+    protected $fillable = [
+        'id_krs',
+        'NIM',
+        'nama',
+        'id_semester',
+        'id_mata_kuliah',
+        'id_kelas',
+        'id_prodi',
+        'nilai_huruf',
+        'nilai_index',
+        'nilai_angka',
+    ];
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class, 'id_prodi', 'id_prodi');
+    }
+
+    public function semester()
+    {
+        return $this->belongsTo(Semester::class, 'id_semester', 'id_semester');
+    }
+
+    public function matkul()
+    {
+        return $this->belongsTo(Matakuliah::class, 'id_mata_kuliah', 'id_mata_kuliah');
+    }
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'id_kelas', 'id_kelas');
+    }
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class, 'NIM', 'NIM');
+    }
+}
