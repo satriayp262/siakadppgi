5d5f42a406db6fc72080fce5541fe5ed7cc543ab
Satria Yudha Pangrangau <127200227+satriayp262@users.noreply.github.com>
2024-11-15 09:58:06 +0700

    Update sidebar-admin.blade.php

--- resources/views/livewire/component/sidebar-admin.blade.php
+++ resources/views/livewire/component/sidebar-admin.blade.php
@@ -28,7 +28,8 @@
                                 d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <ul id="dropdown-sistem" class="hidden py-2 space-y-2">
+                    <ul id="dropdown-sistem"
+                        class="{{ request()->routeIs('admin.user') ? '' : 'hidden' }} py-2 space-y-2">
                         <li>
                             <a href="{{ route('admin.user') }}"
                                 class="flex items-center p-2 text-gray-500 rounded-lg group {{ request()->routeIs('admin.user') ? ' text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-gray-100' }}">
@@ -39,7 +40,6 @@
                                         d="M17 10v1.126c.367.095.714.24 1.032.428l.796-.797 1.415 1.415-.797.796c.188.318.333.665.428 1.032H21v2h-1.126c-.095.367-.24.714-.428 1.032l.797.796-1.415 1.415-.796-.797a3.979 3.979 0 0 1-1.032.428V20h-2v-1.126a3.977 3.977 0 0 1-1.032-.428l-.796.797-1.415-1.415.797-.796A3.975 3.975 0 0 1 12.126 16H11v-2h1.126c.095-.367.24-.714.428-1.032l-.797-.796 1.415-1.415.796.797A3.977 3.977 0 0 1 15 11.126V10h2Zm.406 3.578.016.016c.354.358.574.85.578 1.392v.028a2 2 0 0 1-3.409 1.406l-.01-.012a2 2 0 0 1 2.826-2.83ZM5 8a4 4 0 1 1 7.938.703 7.029 7.029 0 0 0-3.235 3.235A4 4 0 0 1 5 8Zm4.29 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h6.101A6.979 6.979 0 0 1 9 15c0-.695.101-1.366.29-2Z"
                                         clip-rule="evenodd" />
                                 </svg>
-
                                 <span class="flex-1 ms-3 whitespace-nowrap">User</span>
                             </a>
                         </li>
@@ -56,7 +56,7 @@
                                 d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <ul id="dropdown-akademik" class="hidden py-2 space-y-2">
+                    <ul id="dropdown-akademik" class="py-2 space-y-2 {{ request()->routeIs(['admin.kurikulum', 'admin.semester', 'admin.mata_kuliah', 'admin.prodi', 'admin.kelas', 'admin.ruangan']) ? '' : 'hidden' }}">
                         <li>
                             <a href="{{ route('admin.kurikulum') }}"
                                 class="flex items-center p-2 text-gray-500 rounded-lg group {{ request()->routeIs('admin.kurikulum') ? ' text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-gray-100' }}">
@@ -145,13 +145,13 @@
                         class="flex items-center w-full py-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         aria-controls="dropdown-mahasiswa" data-collapse-toggle="dropdown-mahasiswa">
                         <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Manajemen Mahasiswa</span>
-                        <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 10 6">
+                        <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <ul id="dropdown-mahasiswa" class="hidden py-2 space-y-2">
+                    <ul id="dropdown-mahasiswa"  class="py-2 space-y-2 {{ request()->routeIs(['admin.mahasiswa', 'admin.krs']) ? '' : 'hidden' }}">
                         <li>
                             <a href="{{ route('admin.mahasiswa') }}"
                                 class="flex items-center p-2 text-gray-500 rounded-lg group {{ request()->routeIs('admin.mahasiswa') ? ' text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-gray-100' }}">
@@ -186,13 +186,13 @@
                         class="flex items-center w-full py-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         aria-controls="dropdown-dosen" data-collapse-toggle="dropdown-dosen">
                         <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Manajemen Dosen</span>
-                        <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 10 6">
+                        <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <ul id="dropdown-dosen" class="hidden py-2 space-y-2">
+                    <ul id="dropdown-dosen" class="py-2 space-y-2 {{ request()->routeIs('admin.dosen') || request()->routeIs('admin.jadwal') ? 'block' : 'hidden' }}">
                         <li>
                             <a href="{{ route('admin.dosen') }}"
                                 class="flex items-center p-2 text-gray-500 rounded-lg group {{ request()->routeIs('admin.dosen') ? ' text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-gray-100' }}">
