d0a18700f5a5510661fb9f30eca167313059aa53
Fajaraaja <fajardwi881117@gmail.com>
2024-11-28 10:06:41 +0700

    update landing

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -218,21 +218,25 @@
                         </div>
                     </div>
                     <div class="mt-2 relative">
-                        <input
-                            class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            wire:model='password' type="password" name="password" id="password">
-                        <button type="button" onclick="togglePassword('password', this)"
-                            class="absolute inset-y-0 right-0 flex items-center px-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 eye-icon" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke="currentColor" stroke-width="2"
-                                    d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
-                                <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-                        </button>
+                        <div class="relative">
+                            <input
+                                class="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                wire:model="password" type="password" name="password" id="password">
+                            <button type="button" onclick="togglePassword('password', this)"
+                                class="absolute inset-y-0 right-0 flex items-center px-2 h-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 eye-icon" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke="currentColor" stroke-width="2"
+                                        d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                                    <path stroke="currentColor" stroke-width="2"
+                                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </button>
+                        </div>
                         @error('password')
                             <small class="text-red-500">{{ $message }}</small>
                         @enderror
+
                     </div>
                     <div wire:loading>
                         <div class="flex flex-row items-center w-full mt-2 space-x-2">
