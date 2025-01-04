9560ef0c6e4270d35c9e9b1d3bec23ce59835cf7
Fajaraaja <fajardwi881117@gmail.com>
2025-01-04 17:26:20 +0700

    Feat : create transaksi table for payment transactions

---
+++ database/migrations/2025_01_04_102425_create_transaksi_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->integer('id_transaksi', true);
+            $table->integer('NIM');
+            $table->string('snap_token');
+            $table->string('id_tagihan');
+            $table->string('status');
+            $table->string('payment_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
