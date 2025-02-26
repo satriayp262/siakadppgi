fa1ffe6e7a771346a972d476bdfaec33008a25b2
AndikaBastian <andikabastian20@gmail.com>
2025-02-26 16:21:59 +0700

    Update download.blade.php

--- resources/views/livewire/mahasiswa/kartu-ujian/download.blade.php
+++ resources/views/livewire/mahasiswa/kartu-ujian/download.blade.php
@@ -56,93 +56,137 @@
     </div>
 
     <!-- Nama, NIM, dan Prodi di kiri, tabel di kanan -->
-    <div style="display: flex; text-align: center;">
+    <div style="display: flex; margin-top: 2%;">
         
         <!-- Nama, NIM, dan Prodi di kiri -->
-        <div style="text-align: left;">
-            <div style="display: flex;">
-                <span style="width: 4rem; font-weight: 500; color: #4b5563;">Nama</span>
-                <span style="width: 1rem; text-align: center;">:</span>
-                <span style="flex-grow: 1; color: #1f2937;">{{ $mahasiswa->nama }}</span>
-            </div>
-            <div style="display: flex;">
-                <span style="width: 4rem; font-weight: 500; color: #4b5563;">NIM</span>
-                <span style="width: 1rem; text-align: center;">:</span>
-                <span style="flex-grow: 1; color: #1f2937;">{{ $mahasiswa->NIM }}</span>
-            </div>
-            <div style="display: flex;">
-                <span style="width: 4rem; font-weight: 500; color: #4b5563;">Prodi</span>
-                <span style="width: 1rem; text-align: center;">:</span>
-                <span style="flex-grow: 1; color: #1f2937;">{{ $mahasiswa->prodi->nama_prodi }}</span>
-            </div>
-            <div style="display: flex;">
-                <span style="width: 4rem; font-weight: 500; color: #4b5563;">Kelas</span>
-                <span style="width: 1rem; text-align: center;">:</span>
-                <span style="flex-grow: 1; color: #1f2937;">{{ $kelas->kelas->nama_kelas }}</span>
-            </div>
-        </div>
+        <table style="float: left; text-align: left; margin-right: 2%">
 
-        <!-- Tabel di kanan -->
-        <table style="width: 100%; background-color: white; border-collapse: collapse; border: 2px solid black;">
-            <thead>
-                <tr style="text-align: center; font-size: 0.775rem; color: black;">
-                    <th style="padding: 0.5rem; border: 2px solid black;">Hari</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">Sesi</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">Mata Kuliah</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">Dosen</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">Kelas</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">Ruangan</th>
-                    <th style="padding: 0.5rem; border: 2px solid black;">paraf</th>
-                </tr>
-            </thead>
-            <tbody>
                 @php
-                    $previousDay = null;
+                    $nama = $mahasiswa->nama;
+                    $pisah = explode(" ", $nama);
+                    $cont = null;
+                    $cont2 = null;
+                    $cont3 = null;
+
+                    if (count($pisah) > 2) {
+                        $nama = implode(" ", array_slice($pisah, 0, 2));
+                        $cont = implode(" ", array_slice($pisah, 2, 2));
+                        $cont2 = implode(" ", array_slice($pisah, 4, 2));
+                        $cont3 = implode(" ", array_slice($pisah, 6, 2));
+                    }
                 @endphp
 
-                @foreach ($jadwals as $jadwal)
-                    @if ($jadwal->hari != $previousDay)
-                        <tr style="border-top: 2px solid black; font-size: 0.775rem;" wire:key="jadwal-{{ $jadwal->id_jadwal }}">
-                    @else
-                        <tr wire:key="jadwal-{{ $jadwal->id_jadwal }}" style="font-size: 0.775rem;">
-                    @endif
-                        <td style="padding: 0.25rem 0.5rem; text-align: center;">
-                            @if ($jadwal->hari != $previousDay)
-                                <div>
-                                    @if ($jadwal->hari == 'Monday')
-                                        Senin
-                                    @elseif ($jadwal->hari == 'Tuesday')
-                                        Selasa
-                                    @elseif ($jadwal->hari == 'Wednesday')
-                                        Rabu
-                                    @elseif ($jadwal->hari == 'Thursday')
-                                        Kamis
-                                    @elseif ($jadwal->hari == 'Friday')
-                                        Jumat
-                                    @endif
-                                </div>
-                                <div style="font-size: 0.875rem;">
-                                    {{ \Carbon\Carbon::parse($jadwal->tanggal)->locale('id')->isoFormat('D MMMM YYYY') }}
-                                </div>
-                                @php
-                                    $previousDay = $jadwal->hari;
-                                @endphp
-                            @endif
-                        </td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->sesi }}</td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->matakuliah->nama_mata_kuliah }}</td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->dosen->nama_dosen }}</td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->kelas->nama_kelas }}</td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">
-                            {{ $jadwal->id_ruangan == 'Online' ? 'Online' : $jadwal->ruangan->kode_ruangan }}
-                        </td>
-                        <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;"></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            <tr>
+                <td style=" font-weight: 500; color: #4b5563;">Nama</td>
+                <td style=" text-align: center;">:</td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $nama }}</td>
+            </tr>
 
+            @if ($cont)
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $cont }}</td>
+            </tr>
+            @endif
+
+            @if ($cont2)
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $cont2 }}</td>
+            </tr>
+            @endif
+
+            @if ($cont3)
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $cont3 }}</td>
+            </tr>
+            
+            @endif
+            <tr>
+                <td style=" font-weight: 500; color: #4b5563;">NIM</td>
+                <td style=" text-align: center;">:</td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $mahasiswa->NIM }}</td>
+            </tr>
+            <tr>
+                <td style=" font-weight: 500; color: #4b5563;">Prodi</td>
+                <td style=" text-align: center;">:</td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $mahasiswa->prodi->nama_prodi }}</td>
+            </tr>
+            <tr>
+                <td style=" font-weight: 500; color: #4b5563;">Kelas</td>
+                <td style=" text-align: center;">:</td>
+                <td style="flex-grow: 1; color: #1f2937;">{{ $kelas->kelas->nama_kelas }}</td>
+            </tr>
+        </table>
+
+        <!-- Tabel di kanan -->
+        <div style="float: left;">
+            <div style="border-left: 3px solid darkviolet;">
+                <table style="width: 90%; background-color: white; border-collapse: collapse; border: 2px solid black; margin-left: 2%;">
+                    <thead>
+                        <tr style="text-align: center; font-size: 0.775rem; color: black;">
+                            <th style="padding: 0.5rem; border: 2px solid black;">Hari</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">Sesi</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">Mata Kuliah</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">Dosen</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">Kelas</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">Ruangan</th>
+                            <th style="padding: 0.5rem; border: 2px solid black;">paraf</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $previousDay = null;
+                        @endphp
+
+                        @foreach ($jadwals as $jadwal)
+                            @if ($jadwal->hari != $previousDay)
+                                <tr style="border-top: 2px solid black; font-size: 0.775rem;" wire:key="jadwal-{{ $jadwal->id_jadwal }}">
+                            @else
+                                <tr wire:key="jadwal-{{ $jadwal->id_jadwal }}" style="font-size: 0.775rem;">
+                            @endif
+                                <td style="padding: 0.25rem 0.5rem; text-align: center;">
+                                    @if ($jadwal->hari != $previousDay)
+                                        <div>
+                                            @if ($jadwal->hari == 'Monday')
+                                                Senin
+                                            @elseif ($jadwal->hari == 'Tuesday')
+                                                Selasa
+                                            @elseif ($jadwal->hari == 'Wednesday')
+                                                Rabu
+                                            @elseif ($jadwal->hari == 'Thursday')
+                                                Kamis
+                                            @elseif ($jadwal->hari == 'Friday')
+                                                Jumat
+                                            @endif
+                                        </div>
+                                        <div style="font-size: 0.875rem;">
+                                            {{ \Carbon\Carbon::parse($jadwal->tanggal)->locale('id')->isoFormat('D MMMM YYYY') }}
+                                        </div>
+                                        @php
+                                            $previousDay = $jadwal->hari;
+                                        @endphp
+                                    @endif
+                                </td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->sesi }}</td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->matakuliah->nama_mata_kuliah }}</td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->dosen->nama_dosen }}</td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">{{ $jadwal->kelas->nama_kelas }}</td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;">
+                                    {{ $jadwal->id_ruangan == 'Online' ? 'Online' : $jadwal->ruangan->kode_ruangan }}
+                                </td>
+                                <td style="padding: 0.25rem 0.5rem; text-align: center; border: 2px solid black;"></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
