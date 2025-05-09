0ebbd3120e1678b2b291aa6a674bb36143c9d6db
Fajaraaja <fajardwi881117@gmail.com>
2025-05-09 08:23:06 +0700

    Refactor : add datattable to periode table

--- app/Livewire/Dosen/Emonev/Show.php
+++ app/Livewire/Dosen/Emonev/Show.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dosen\Emonev;
 
+use App\Models\Matakuliah;
 use Livewire\Component;
 use App\Models\Jawaban;
 use App\Models\Pertanyaan;
@@ -25,6 +26,13 @@ class Show extends Component
         $this->semesters = Semester::orderBy('id_semester', 'desc')->get();
         $this->loadData();
     }
+    public function setValues($nilai, $pertanyaanId)
+    {
+        $this->selectedNilai = $nilai;
+        $this->selectedPertanyaan = $pertanyaanId;
+
+    }
+
     public function loadData()
     {
         // Ambil semua pertanyaan agar bisa dijadikan header
@@ -48,14 +56,13 @@ class Show extends Component
             $query->where('periode_emonev.id_semester', $findsemester->id_semester);
 
         } else {
+
             $query->where('periode_emonev.id_semester', $this->semesters[0]->id_semester);
 
         }
 
         $query->where('dosen.nidn', auth()->user()->nim_nidn);
         $query->where('matkul.id_mata_kuliah', $this->id);
-
-
 
 
         if ($this->selectedNilai && $this->selectedPertanyaan) {
@@ -67,20 +74,14 @@ class Show extends Component
             }
         }
 
-        // Tambahkan kolom rata-rata untuk setiap pertanyaan
+
         foreach ($pertanyaan as $p) {
-            $query->addSelect(DB::raw(
-                "
-            (SELECT AVG(jwbn.nilai) 
-             FROM jawaban jwbn 
-             JOIN emonev em ON jwbn.id_emonev = em.id_emonev
-             WHERE em.nidn = dosen.nidn 
-             AND jwbn.id_pertanyaan = $p->id_pertanyaan
-            ) AS `pertanyaan_$p->id_pertanyaan`"
-            ));
+            $query->addSelect(DB::raw("
+        MAX(CASE WHEN jawaban.id_pertanyaan = {$p->id_pertanyaan} THEN jawaban.nilai END) AS pertanyaan_{$p->id_pertanyaan}
+    "));
         }
 
-        $this->jawaban = $query->groupBy(
+        $jawaban = $query->groupBy(
             'dosen.nidn',
             'dosen.nama_dosen',
             'matkul.nama_mata_kuliah',
@@ -88,17 +89,72 @@ class Show extends Component
             'periode_emonev.nama_periode',
         )->get();
 
-        return $query;
+        return $jawaban;
     }
 
     public function render()
     {
         $pertanyaan = Pertanyaan::all();
+        $matakuliah = Matakuliah::where('id_mata_kuliah', $this->id)->first()->value('nama_mata_kuliah');
+        // Ambil semua pertanyaan agar bisa dijadikan header
+        $pertanyaan = Pertanyaan::all();
+        $query = Jawaban::join('emonev', 'jawaban.id_emonev', '=', 'emonev.id_emonev')
+            ->join('pertanyaan', 'jawaban.id_pertanyaan', '=', 'pertanyaan.id_pertanyaan')
+            ->join('kelas', 'emonev.id_kelas', '=', 'kelas.id_kelas')
+            ->join('matkul', 'emonev.id_mata_kuliah', '=', 'matkul.id_mata_kuliah')
+            ->join('dosen', 'matkul.nidn', '=', 'dosen.nidn')
+            ->join('periode_emonev', 'emonev.nama_periode', '=', 'periode_emonev.nama_periode')
+            ->select(
+                'dosen.nidn',
+                'dosen.nama_dosen',
+                'emonev.saran',
+                'periode_emonev.nama_periode',
+                'matkul.nama_mata_kuliah',
+            );
+
+        if (!empty($this->selectedSemester)) {
+            $findsemester = Semester::where('nama_semester', $this->selectedSemester)->first();
+            $query->where('periode_emonev.id_semester', $findsemester->id_semester);
+
+        } else {
+
+            $query->where('periode_emonev.id_semester', $this->semesters[0]->id_semester);
+
+        }
+
+        $query->where('dosen.nidn', auth()->user()->nim_nidn);
+        $query->where('matkul.id_mata_kuliah', $this->id);
+
+
+        if ($this->selectedNilai && $this->selectedPertanyaan) {
+            $query->where('pertanyaan.id_pertanyaan', $this->selectedPertanyaan);
+            $query->where('jawaban.nilai', $this->selectedNilai);
+            if ($query->count() == 0) {
+                $this->dispatch('warning', ['message' => 'Data tidak ditemukan']);
+                return $this->selectedNilai = '' && $this->selectedPertanyaan = '';
+            }
+        }
+
+
+        foreach ($pertanyaan as $p) {
+            $query->addSelect(DB::raw("
+        MAX(CASE WHEN jawaban.id_pertanyaan = {$p->id_pertanyaan} THEN jawaban.nilai END) AS pertanyaan_{$p->id_pertanyaan}
+    "));
+        }
+
+        $jawaban = $query->groupBy(
+            'dosen.nidn',
+            'dosen.nama_dosen',
+            'matkul.nama_mata_kuliah',
+            'emonev.saran',
+            'periode_emonev.nama_periode',
+        )->paginate(10);
 
         return view('livewire.dosen.emonev.show', [
-            'jawaban' => $this->jawaban,
+            'jawaban' => $jawaban,
             'semesters' => $this->semesters,
             'pertanyaan' => $pertanyaan,
+            'Matakuliah' => $matakuliah,
         ]);
     }
 }
