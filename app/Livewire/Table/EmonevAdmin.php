301b5e4ada1a62ea6e3780468bae81e6ffee7235
Fajaraaja <fajardwi881117@gmail.com>
2025-05-09 14:55:03 +0700

    Refactor : update EmonevAdmin component and integrate OpenSpout for spreadsheet handling
    composer install

--- app/Livewire/Table/EmonevAdmin.php
+++ app/Livewire/Table/EmonevAdmin.php
@@ -2,33 +2,79 @@
 
 namespace App\Livewire\Table;
 
-use Illuminate\Support\Carbon;
+use App\Models\Jawaban;
+use App\Models\PeriodeEMonev;
+use App\Models\Pertanyaan;
+use App\Models\Prodi;
 use Illuminate\Support\Collection;
 use PowerComponents\LivewirePowerGrid\Column;
 use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
 use PowerComponents\LivewirePowerGrid\PowerGridFields;
+use PowerComponents\LivewirePowerGrid\Traits\WithExport;
+use PowerComponents\LivewirePowerGrid\Components\SetUp\Exportable;
 use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+
+use Illuminate\Support\Facades\DB;
 
 final class EmonevAdmin extends PowerGridComponent
 {
     public string $tableName = 'emonev-admin-jfjqjo-table';
+    use WithExport;
 
     public function datasource(): Collection
     {
-        return collect([
-            ['id' => 1, 'name' => 'Name 1', 'price' => 1.58, 'created_at' => now(),],
-            ['id' => 2, 'name' => 'Name 2', 'price' => 1.68, 'created_at' => now(),],
-            ['id' => 3, 'name' => 'Name 3', 'price' => 1.78, 'created_at' => now(),],
-            ['id' => 4, 'name' => 'Name 4', 'price' => 1.88, 'created_at' => now(),],
-            ['id' => 5, 'name' => 'Name 5', 'price' => 1.98, 'created_at' => now(),],
-        ]);
+        $pertanyaan = Pertanyaan::all();
+        $query = Jawaban::join('emonev', 'jawaban.id_emonev', '=', 'emonev.id_emonev')
+            ->join('pertanyaan', 'jawaban.id_pertanyaan', '=', 'pertanyaan.id_pertanyaan')
+            ->join('kelas', 'emonev.id_kelas', '=', 'kelas.id_kelas')
+            ->join('matkul', 'emonev.id_mata_kuliah', '=', 'matkul.id_mata_kuliah')
+            ->join('dosen', 'matkul.nidn', '=', 'dosen.nidn')
+            ->join('periode_emonev', 'emonev.nama_periode', '=', 'periode_emonev.nama_periode')
+            ->join('prodi', 'matkul.kode_prodi', '=', 'prodi.kode_prodi')
+            ->select(
+                'dosen.nidn',
+                'dosen.nama_dosen',
+                'prodi.nama_prodi',
+                'periode_emonev.nama_periode',
+                'matkul.nama_mata_kuliah',
+            );
+
+        foreach ($pertanyaan as $p) {
+            $query->addSelect(DB::raw(
+                "
+            (SELECT ROUND(AVG(jwbn.nilai), 0) 
+             FROM jawaban jwbn 
+             JOIN emonev em ON jwbn.id_emonev = em.id_emonev
+             WHERE em.nidn = dosen.nidn 
+             AND jwbn.id_pertanyaan = $p->id_pertanyaan
+            ) AS `pertanyaan_$p->id_pertanyaan`"
+            ));
+        }
+
+        $query->groupBy(
+            'dosen.nidn',
+            'dosen.nama_dosen',
+            'matkul.nama_mata_kuliah',
+            'prodi.nama_prodi',
+            'periode_emonev.nama_periode',
+        )->get();
+
+        $query = $query->get();
+
+        return $query;
     }
 
     public function setUp(): array
     {
-        $this->showCheckBox();
 
         return [
+
+            PowerGrid::exportable(fileName: 'emonev-file')
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV)
+                ->csvSeparator(separator: '|')
+                ->csvDelimiter(delimiter: "'"),
+
             PowerGrid::header()
                 ->showSearchInput(),
             PowerGrid::footer()
@@ -37,34 +83,96 @@ final class EmonevAdmin extends PowerGridComponent
         ];
     }
 
+    public function relationSearch(): array
+    {
+        return [
+            'dosen' => [
+                'nidn',
+                'nama_dosen',
+            ],
+            'prodi' => [
+                'kode_prodi',
+                'nama_prodi',
+            ],
+            'periode_emonev' => [
+                'nama_periode',
+            ],
+            'matkul' => [
+                'id_mata_kuliah',
+                'nama_mata_kuliah',
+            ],
+        ];
+    }
+
+
     public function fields(): PowerGridFields
     {
-        return PowerGrid::fields()
+        $fields = PowerGrid::fields()
             ->add('id')
-            ->add('name')
-            ->add('price')
-            ->add('created_at_formatted', function ($entry) {
-                return Carbon::parse($entry->created_at)->format('d/m/Y');
-            });
+            ->add('periode_emonev.nama_periode')
+            ->add('dosen.nama_dosen')
+            ->add('prodi.nama_prodi')
+            ->add('matkul.nama_mata_kuliah');
+
+        // Tambahkan semua field pertanyaan_X secara dinamis
+        foreach (Pertanyaan::all() as $p) {
+            $fields->add("pertanyaan_$p->id_pertanyaan");
+        }
+
+        return $fields;
     }
 
     public function columns(): array
     {
+        $columns = [];
+
+        // Kolom dinamis untuk setiap pertanyaan
+        foreach (Pertanyaan::all() as $p) {
+            $columns[] = Column::make($p->pertanyaan ?? "$p->nama_pertanyaan", "pertanyaan_$p->id_pertanyaan")
+                ->sortable()
+                ->searchable();
+        }
+
+        return array_merge([
+            Column::make('ID', 'id')->visibleInExport(false)->index(),
+
+            Column::make('Periode', 'nama_periode')
+                ->searchable()
+                ->sortable(),
+
+            Column::make('Dosen', 'nama_dosen')
+                ->searchable()
+                ->sortable(),
+
+            Column::make('Prodi', 'nama_prodi')
+                ->searchable()
+                ->sortable(),
+
+            Column::make('Mata Kuliah', 'nama_mata_kuliah')
+                ->searchable()
+                ->sortable(),
+
+        ], $columns);
+    }
+
+    public function filters(): array
+    {
         return [
-            Column::make('ID', 'id')
-                ->searchable()
-                ->sortable(),
+            Filter::select('nama_periode', 'nama_periode')
+                ->dataSource(PeriodeEMonev::all()->map(fn($periode) => [
+                    'value' => $periode->nama_periode,
+                    'label' => $periode->nama_periode,
+                ]))
+                ->optionLabel('label')
+                ->optionValue('value'),
 
-            Column::make('Name', 'name')
-                ->searchable()
-                ->sortable(),
-
-            Column::make('Price', 'price')
-                ->sortable(),
-
-            Column::make('Created', 'created_at_formatted'),
-
-            Column::action('Action')
+            Filter::select('nama_prodi', 'nama_prodi')
+                ->dataSource(Prodi::all()->map(fn($prodi) => [
+                    'value' => $prodi->nama_prodi,
+                    'label' => $prodi->nama_prodi,
+                ]))
+                ->optionLabel('label')
+                ->optionValue('value'),
         ];
     }
 }
