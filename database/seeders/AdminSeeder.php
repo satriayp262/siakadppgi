56539155cb902d020868da1e6f97b77f25155384
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2025-07-17 11:13:15 +0700

    Create AdminSeeder.php

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => '123@gmail.com',
+            'password' => '11111111',
+            'role' => 'admin',
+        ]);
+    }
+}
