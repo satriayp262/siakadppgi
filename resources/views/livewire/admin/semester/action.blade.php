b529f33b557ddaae2ddaa9b09efd913d5f8a6992
Fajaraaja <fajardwi881117@gmail.com>
2025-07-19 17:10:00 +0700

    Fix : Update active function parameter and button onclick handler

--- resources/views/livewire/admin/semester/action.blade.php
+++ resources/views/livewire/admin/semester/action.blade.php
@@ -1,6 +1,6 @@
 @if ($row->is_active != 1)
     <button class="inline-block px-4 py-1 text-white bg-green-500 rounded hover:bg-green-700"
-        onclick="confirmActive({{ rand() . $row->id_semester }})">
+        onclick="confirmActive({{ $row->id_semester }})">
         <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
             fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
