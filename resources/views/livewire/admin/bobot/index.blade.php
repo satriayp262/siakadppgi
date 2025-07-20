ae2777644ef70f36113219ec2e3bede87722beb1
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2025-07-20 09:50:09 +0700

    Revert "cek"
    This reverts commit 49456975450d8515a70a37bbc789bb9129d6530e.

--- resources/views/livewire/admin/bobot/index.blade.php
+++ resources/views/livewire/admin/bobot/index.blade.php
@@ -8,7 +8,7 @@
                         <div class="flex items-center">
                             <a wire:navigate.hover  href="{{ route('admin.bobot') }}"
                                 class="text-sm font-medium text-gray-500 hover:text-gray-700 flex items-center">
-                                <span class="text-sm font-medium text-gray-500 ms-1 md:ms-2">cek Bobot</span>
+                                <span class="text-sm font-medium text-gray-500 ms-1 md:ms-2">Bobot</span>
                                 <svg class="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
